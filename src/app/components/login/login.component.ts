b9073e54cbf82bd8069046a68725d48ad6b1546b
superKalo <superkalo@devlabs.bg>
2016-11-04 18:04:33 +0200

    Added: moving the auth login logic; Added: routing Added: bootstrap login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'xp-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
