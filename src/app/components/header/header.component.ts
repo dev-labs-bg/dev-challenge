0311d4915aee4ce9df2745ba604da32fa7069085
Borislav Itskov <borislav@devlabs.bg>
2016-11-07 13:14:11 +0200

    add: missing header files;

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'xp-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  title = 'Dev Challenge';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
