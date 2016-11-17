3ef945b9931a799ca189be26d92f01035b1fc044
superKalo <superkalo@devlabs.bg>
2016-11-17 17:22:27 +0200

    Changed: close create form after successful submission

--- src/app/admin/categories/create.component.ts
+++ src/app/admin/categories/create.component.ts
@@ -35,6 +35,8 @@ export class CreateComponent {
                             response.category.name
                         )
                     );
+
+                    this.onCancel.emit();
                 }
             }
         );
