599b6ddadb534135bee24e0c2865a5804205393d
superKalo <superkalo@devlabs.bg>
2016-11-25 20:07:51 +0200

    Added: count words and implement words limit

--- src/app/assessment/micro-project/form.component.ts
+++ src/app/assessment/micro-project/form.component.ts
@@ -7,11 +7,22 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
         <form [formGroup]="form" (ngSubmit)="handleSubmit()">
             <div class="form-group">
                 <textarea
+                    #message
                     required
                     rows="6"
                     formControlName="message"
                     class="form-control"></textarea>
             </div>
+            <p
+                class="text-right"
+                [ngClass]="{
+                    'text-danger': countWords(message.value) > wordsLimit,
+                    'text-success': countWords(message.value) <= wordsLimit
+                }">
+                <strong>
+                    {{ wordsLimit - countWords(message.value) }} words left
+                </strong>
+            </p>
             <button
                 type="submit"
                 class="btn btn-primary"
@@ -24,14 +35,51 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class AssessmentMicroProjectFormComponent implements OnInit {
     @Output() onSubmit = new EventEmitter();
-    form: FormGroup;
+    private form: FormGroup;
+    private wordsLimit = 110;
 
     constructor(private formBuilder: FormBuilder) { }
 
     ngOnInit() {
         this.form = this.formBuilder.group({
             'message': ['', Validators.required]
+        }, {
+            validator: this.messageMaxLength(this.wordsLimit)
         });
+    }
+
+    private messageMaxLength(maxWords) {
+        return (group: FormGroup): {[key: string]: any} => {
+            const { message } = group.controls;
+
+            if (this.countWords(message.value) > maxWords) {
+                return {
+                    mismatchedWordsCount: true
+                };
+            }
+        };
+
+    }
+
+    /**
+     * Count words in any string, see:
+     * http://stackoverflow.com/a/18679657/1333836
+     *
+     * @param {string} s
+     */
+    private countWords(s) {
+        if (! s.trim().length) {
+            return 0;
+        }
+
+        // exclude  start and end white-space
+        s = s.replace(/(^\s*)|(\s*$)/gi, '');
+        // 2 or more space to 1
+        s = s.replace(/[ ]{2,}/gi, ' ');
+        // exclude newline with a start spacing
+        s = s.replace(/\n /, '\n');
+
+        return s.split(' ').length;
     }
 
     handleSubmit() {
