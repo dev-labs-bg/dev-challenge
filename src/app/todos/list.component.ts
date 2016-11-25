44da17f34441f88d0de43cd6c8b16c30acfb6b05
superKalo <superkalo@devlabs.bg>
2016-11-25 15:20:18 +0200

    Added: loading indicator

--- src/app/todos/list.component.ts
+++ src/app/todos/list.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
 
 import { TodoService } from './todo.service';
 
 @Component({
     selector: 'xp-todos-list',
     template: `
-        <div class="list-group">
-            <div
-                *ngFor="let todo of todoService.repository.getData()"
-                class="list-group-item">
-                <xp-todo-item [todo]="todo"></xp-todo-item>
+        <xp-loading-indicator [wait]="subscription">
+            <div class="list-group">
+                <div
+                    *ngFor="let todo of todoService.repository.getData()"
+                    class="list-group-item">
+                    <xp-todo-item [todo]="todo"></xp-todo-item>
+                </div>
             </div>
-        </div>
+        </xp-loading-indicator>
     `,
     styles: []
 })
 export class TodosListComponent implements OnInit {
+    private subscription: Subscription;
 
     constructor(private todoService: TodoService) { }
 
     ngOnInit() {
-        this.todoService.setup();
+        this.subscription = this.todoService.setup();
     }
 
 }
