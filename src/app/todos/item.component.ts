56e87adf9319b58c4e8d77258eb21896f2934f33
superKalo <superkalo@devlabs.bg>
2016-11-25 18:47:15 +0200

    Added: approval flag ui

--- src/app/todos/item.component.ts
+++ src/app/todos/item.component.ts
@@ -5,12 +5,17 @@ import { Todo } from './todo';
     selector: 'xp-todo-item',
     template: `
         <h4 class="list-group-item-heading">
-            <input type="checkbox" />
-            <span
-                *ngIf="todo.completed"
-                class="label label-success">
+            <span *ngIf="todo.completed" class="text-success">
+                <i class="glyphicon glyphicon-check"></i>
                 completed
             </span>
+            <span *ngIf="! todo.completed">
+                <span
+                    class="label label-warning">
+                    submitted for review
+                </span>
+            </span>
+            &nbsp;
             {{ todo.title }} | <span
                 class="label label-info">
                 3 days remaining
