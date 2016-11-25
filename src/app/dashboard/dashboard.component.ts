0bd3746bcff9a09550698a109d0d02b52d3292cd
superKalo <superkalo@devlabs.bg>
2016-11-25 13:45:04 +0200

    Added: main UI

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'xp-dashboard',
     template: `
-        dashboard works.
+        <div class="list-group">
+            <div class="list-group-item">
+                <h4 class="list-group-item-heading">
+                    <input type="checkbox" />
+                    <span class="label label-success">completed</span>
+                    TODO title
+                </h4>
+                <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit voluptates porro repudiandae quaerat neque culpa distinctio ratione. Dolor, optio. Deleniti totam aut at aliquid nihil voluptate magnam possimus accusamus fugiat alias cum non vero quasi quis iure quia mollitia officia maxime blanditiis ex ipsum, rerum sint. Nam sapiente odio laboriosam. Veniam debitis quae tempore magni alias provident sunt accusamus non, facere necessitatibus dignissimos quasi nesciunt adipisci atque molestias enim expedita voluptatum dolores voluptatem dolor, tempora commodi architecto dicta! Cumque eaque nemo, in eos quaerat maxime asperiores ullam officiis ad perferendis dolorem ab voluptas eum vero veritatis sequi reiciendis quasi laborum.</p>
+            </div>
+        </div>
     `
 })
 export class DashboardComponent implements OnInit {
