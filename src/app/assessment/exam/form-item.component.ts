5de5f3b92f63fc25ee2e34ab8eed63b9ac9f2a9d
superKalo <superkalo@devlabs.bg>
2016-12-08 19:07:12 +0200

    Added: save answer

--- src/app/assessment/exam/form-item.component.ts
+++ src/app/assessment/exam/form-item.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
 import * as _ from 'lodash';
 
 import { Answer } from '../assessment';
+import { AssessmentService } from '../assessment.service';
 
 @Component({
     selector: 'xp-assessment-exam-form-item',
@@ -10,36 +12,39 @@ import { Answer } from '../assessment';
             Question {{ questionNumber }} out of {{ questionsCount }}
         </p>
         <h2>{{ question.body }}</h2>
-        <button
-            *ngFor="let answer of question.answers"
-            (click)="handleAnswerSubmit(answer)"
-            [disabled]="isAnswerChosen()"
-            type="button"
-            class="btn btn-default"
-            [ngClass]="{
-                'btn-success': showCorrectAnswer(answer),
-                'btn-primary': answer === chosenAnswer
-            }">
-            {{ answer.body }}
-        </button>
 
-        <div [ngSwitch]="mode">
-            <div *ngSwitchCase="modes.CORRECT_ANSWER" class="alert alert-success">
-                Correct answer!<br />
-                {{ whyCorrect }}
-            </div>
-            <div *ngSwitchCase="modes.WRONG_ANSWER" class="alert alert-danger">
-                Wrong answer!<br />
-                {{ whyCorrect }}
-            </div>
-        </div>
+        <xp-loading-indicator [wait]="subscription">
+            <button
+                *ngFor="let answer of question.answers"
+                (click)="handleAnswerSubmit(answer)"
+                [disabled]="isAnswerChosen()"
+                type="button"
+                class="btn btn-default"
+                [ngClass]="{
+                    'btn-success': showCorrectAnswer(answer),
+                    'btn-primary': answer === chosenAnswer
+                }">
+                {{ answer.body }}
+            </button>
 
-        <button
-            *ngIf="isAnswerChosen()"
-            (click)="handleNext()"
-            class="btn btn-primary">
-            {{ isLastQuestion() ? 'Finish!' : 'Next!' }}
-        </button>
+            <div [ngSwitch]="mode">
+                <div *ngSwitchCase="modes.CORRECT_ANSWER" class="alert alert-success">
+                    Correct answer!<br />
+                    {{ whyCorrect }}
+                </div>
+                <div *ngSwitchCase="modes.WRONG_ANSWER" class="alert alert-danger">
+                    Wrong answer!<br />
+                    {{ whyCorrect }}
+                </div>
+            </div>
+
+            <button
+                *ngIf="isAnswerChosen()"
+                (click)="handleNext()"
+                class="btn btn-primary">
+                {{ isLastQuestion() ? 'Finish!' : 'Next!' }}
+            </button>
+        </xp-loading-indicator>
     `,
     styles: []
 })
@@ -58,8 +63,9 @@ export class AssessmentExamFormItemComponent implements OnInit {
         WRONG_ANSWER: 2
     };
     private mode = this.modes.CHOOSE_ANSWER;
+    private subscription: Subscription;
 
-    constructor() { }
+    constructor(private assessmentService: AssessmentService) { }
 
     ngOnInit() {
         /**
@@ -74,18 +80,23 @@ export class AssessmentExamFormItemComponent implements OnInit {
     }
 
     handleAnswerSubmit(answer) {
+        const answerId = answer.id;
         this.chosenAnswer = answer;
 
-        const answerId = answer.id;
-        console.log('todoId', this.todoId);
-        console.log('questionId', this.questionId);
-        console.log('answerId', answerId);
-
-        if (answer.is_correct) {
-            this.mode = this.modes.CORRECT_ANSWER;
-        } else {
-            this.mode = this.modes.WRONG_ANSWER;
-        }
+        this.subscription = this.assessmentService.submitOpenQuestionAnswer(
+            this.todoId,
+            this.questionId,
+            answerId
+        ).subscribe(
+            response => {
+                if (answer.is_correct) {
+                    this.mode = this.modes.CORRECT_ANSWER;
+                } else {
+                    this.mode = this.modes.WRONG_ANSWER;
+                }
+            },
+            error => console.log('Ah, assessment not submitted!', error)
+        );
     }
 
     isAnswerChosen() {
