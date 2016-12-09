58ab4ee291c34a0a605a345a398037d459718f3f
superKalo <superkalo@devlabs.bg>
2016-12-09 20:47:22 +0200

    Added: timer in progress

--- src/app/shared/audio-recorder.component.ts
+++ src/app/shared/audio-recorder.component.ts
@@ -10,6 +10,7 @@
  *  - https://github.com/zhuker/lamejs
  */
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 import { NotificationService } from './notification.service';
 
@@ -20,7 +21,11 @@ const MediaStreamRecorder = require('msr');
     selector: 'xp-audio-recorder',
     template: `
         <button *ngIf="mode === modes.STAND_BY" class="btn btn-primary" (click)="start()">Start</button>
-        <button *ngIf="mode === modes.IN_PROGRESS" class="btn btn-default" (click)="stop()">Stop</button>
+
+        <div *ngIf="mode === modes.IN_PROGRESS">
+            <button class="btn btn-default" (click)="stop()">Stop</button>
+            / {{ timerLimit - timerValue }} seconds remaining
+        </div>
 
         <div *ngIf="mode === modes.RECORDING_MADE">
             <button class="btn btn-default" (click)="play()">Play</button>
@@ -44,11 +49,13 @@ export class AudioRecorderComponent implements OnInit {
         RECORDING_MADE: 'RECORDING_MADE'
     };
     private mode = this.modes.STAND_BY;
+    private timerValue: number = 0;
+    private timer: Subscription;
+    private timerLimit: number = 5;
 
     constructor(private notificationService: NotificationService) { }
 
     ngOnInit() {
-
     }
 
     toggleMode(nextMode) {
@@ -63,7 +70,7 @@ export class AudioRecorderComponent implements OnInit {
                 this.audio = blob;
                 this.blobURL = URL.createObjectURL(blob);
             };
-            this.mediaRecorder.start(60 * 1000);
+            this.mediaRecorder.start(this.timerLimit * 1000);
         };
 
         const onMediaError = e => this.notificationService.fireError(
@@ -72,6 +79,8 @@ export class AudioRecorderComponent implements OnInit {
 
         navigator.getUserMedia(this.mediaConstraints, onMediaSuccess, onMediaError);
         this.toggleMode(this.modes.IN_PROGRESS);
+
+        this.startTimer(this.timerLimit * 1000);
     }
 
     stop() {
@@ -92,6 +101,23 @@ export class AudioRecorderComponent implements OnInit {
 
     upload() {
         this.onUpload.emit(this.audio);
+    }
+
+    clearTimer() {
+        this.timerValue = 0;
+    }
+
+    startTimer(duration) {
+        this.clearTimer();
+
+        this.timer = Observable.timer(0, 1000)
+            .subscribe( t => {
+                this.timerValue = t;
+
+                if (this.timerValue * 1000 >= duration) {
+                    this.timer.unsubscribe();
+                }
+            });
     }
 
 }
