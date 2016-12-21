fecba62d8fc3d4f3832962767a79c61df39765db
superKalo <superkalo@devlabs.bg>
2016-12-21 20:45:39 +0200

    Changed: improve UI/UX; Added: validate email; Changed: better validation messsages;

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,106 +1,90 @@
 import {Component, OnInit} from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import {Http, Response, Headers, URLSearchParams} from '@angular/http';
-import {Router} from '@angular/router';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 
 import { AuthService } from '../core/auth.service';
-import { HttpService } from '../services/http.service';
+import Utils from '../shared/utils';
 
 @Component({
     selector: 'xp-login',
     template: `
-        <div class="center-block login-form">
-            <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
-                <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input
-                        type="email"
-                        name="email"
-                        class="form-control"
-                        id="email"
-                        formControlName="email"
-                        placeholder="Email"
-                        required
-                    />
-                    <div
-                        *ngIf="loginForm.controls['email'].hasError('required') &&
-                        loginForm.controls['email'].touched"
-                        class="alert alert-danger">
-                        You must specify an email.
+        <div class="row">
+            <div class="col-sm-8 col-sm-push-2">
+                <h3>Вхoд</h3>
+                <form [formGroup]="form" (ngSubmit)="onSubmit()">
+                    <div class="form-group"
+                        [class.has-error]="
+                            ! form.controls['email'].valid &&
+                            form.controls['email'].touched
+                        ">
+                        <input
+                            #email
+                            type="email"
+                            name="email"
+                            class="form-control"
+                            formControlName="email"
+                            placeholder="Email"
+                            required
+                        />
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        class="form-control"
-                        id="password"
-                        formControlName="password"
-                        placeholder="Password"
-                        required
-                    />
-                    <div
-                        *ngIf="loginForm.controls['password'].hasError('required') &&
-                        loginForm.controls['password'].touched"
-                        class="alert alert-danger">
-                        You must specify a password
+                    <div class="form-group"
+                        [class.has-error]="
+                            ! form.controls['password'].valid &&
+                            form.controls['password'].touched
+                        ">
+                        <input
+                            type="password"
+                            name="password"
+                            class="form-control"
+                            formControlName="password"
+                            placeholder="Password"
+                            required
+                        />
                     </div>
-                </div>
-                <button
-                    type="submit"
-                    class="btn btn-default"
-                    [disabled]="!loginForm.valid">
-                    Submit
-                </button>
-                <span>OR</span>
-                <a href="http://dev-challenge.dev/auth/github">
-                    Login with github
-                </a>
-            </form>
+                    <button
+                        type="submit"
+                        class="btn btn-primary btn-lg"
+                        [disabled]="! form.valid">
+                        Хайде!
+                    </button>
+                    <a
+                        href="http://dev-challenge.dev/auth/github"
+                        class="btn btn-link btn-lg">
+                        ... or login with github
+                    </a>
+                </form>
+            </div>
         </div>
-    `,
-    styles: [`
-        .login-form {
-            width: 600px;
-        }
-    `]
+    `
 })
 export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
+    form: FormGroup;
 
     constructor(
         private formBuilder: FormBuilder,
-        private authService: AuthService,
-        private httpService: HttpService,
-        private http: Http,
+        private authService: AuthService
     ) {}
 
     ngOnInit() {
         // init form and set rules
-        this.loginForm = this.formBuilder.group({
-            'email': [null, Validators.required],
-            'password': [null, Validators.required]
+        this.form = this.formBuilder.group({
+            'email': ['', Validators.compose([
+                Validators.required, this.validatorMailFormat
+            ])],
+            'password': ['', Validators.required]
         });
     }
 
-    // login form submission
-    onSubmit() {
-        // get form values
-        const { email, password } = this.loginForm.value;
+    validatorMailFormat(control: FormControl) {
+        if (! Utils.isValidEmail(control.value)) {
+            return { 'incorrectMailFormat': true };
+        }
 
-        // call login
-        this.authService.login(email, password);
+        return null;
     }
 
-    // githubLogin() {
-    //     this.httpService.get('auth/github').subscribe(
-    //         response => this.http.get(response.data)
-    //             .map((response: Response) => response.json())
-    //             .subscribe(
-    //                 data => console.log(data)
-    //             )
-    //     );
-    // }
+    onSubmit() {
+        const { email, password } = this.form.value;
 
+        this.authService.login(email, password);
+    }
 }
