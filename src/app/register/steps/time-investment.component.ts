2bfc2b4d004b258cef3112e6fa30f2dc293be974
superKalo <superkalo@devlabs.bg>
2016-12-22 17:31:20 +0200

    Changed: refine time investment screen

--- src/app/register/steps/time-investment.component.ts
+++ src/app/register/steps/time-investment.component.ts
@@ -7,37 +7,52 @@ import { CategoryService } from '../../admin/categories/category.service';
     selector: 'xp-register-time-investment',
     template: `
         <form [formGroup]="form" (ngSubmit)="handleSubmit()">
-            <div class="form-group">
-                <div class="form-group">
-                    <label for="category_id">Category you wish to specialize in</label>
-                    <p>Choose carefully! All the tasks that will be assigned to you 
-                    will be from the chosen category</p>
-                    <select
-                        name="category_id"
-                        formControlName="category_id"
-                        class="form-control"
-                        id="category_id">
-                        <option *ngFor="let category of categoryService.getCategories()"
-                            value="{{ category.id }}">
-                            {{ category.name }}
-                        </option>
-                    </select>
-                </div>
-                <label for="spent_time">Hours per day you could spend on dev-challenge</label>
-                <input
-                    type="spent_time"
-                    name="spent_time"
+            <div class="form-group mb"
+                [class.has-error]="
+                    ! form.controls['category_id'].valid &&
+                    form.controls['category_id'].touched
+                ">
+                <label for="category_id">Category you wish to specialize in</label>
+                <select
+                    name="category_id"
+                    formControlName="category_id"
                     class="form-control"
-                    id="spent_time"
+                    id="category_id">
+                    <option value="" disabled="true" [selected]="true">Please Choose</option>
+                    <option *ngFor="let category of categoryService.getCategories()"
+                        value="{{ category.id }}">
+                        {{ category.name }}
+                    </option>
+                </select>
+                <p class="help-block">
+                    Choose carefully! All the tasks that will be assigned to you
+                    will be from the chosen category
+                </p>
+            </div>
+            <div class="form-group"
+                [class.has-error]="
+                    ! form.controls['spent_time'].valid &&
+                    form.controls['spent_time'].touched
+                ">
+                <label for="spent_time">Hours per day you could invest?</label>
+                <select
+                    name="spent_time"
                     formControlName="spent_time"
-                    placeholder="1"
-                />
-                <div
-                    *ngIf="form.controls['spent_time'].hasError('required') &&
-                    form.controls['spent_time'].touched"
-                    class="alert alert-danger">
-                        You must fill in your available hours.
-                </div>
+                    class="form-control"
+                    id="spent_time">
+                    <option value="" disabled="true" [selected]="true">Please Choose</option>
+                    <option *ngFor="let tileSlot of tileSlots"
+                        value="{{ tileSlot }}">
+                        {{ tileSlot }} hours
+                    </option>
+                    <option value="tileSlots[tileSlots.length - 1]">
+                        More then {{ tileSlots[tileSlots.length - 1] }} hours
+                    </option>
+                </select>
+                <p class="help-block">
+                    Choose carefully! Time estimations for each task
+                    will be aligned with the amount of time you can invest!
+                </p>
             </div>
             <button
                 type="submit"
@@ -46,14 +61,14 @@ import { CategoryService } from '../../admin/categories/category.service';
                 Next
             </button>
         </form>
-    `,
-    styles: []
+    `
 })
 export class TimeInvestmentComponent implements OnInit {
     @Output() onSubmit = new EventEmitter();
     form: FormGroup;
     private currentDate = new Date();
     private maxDate: Date = new Date();
+    private tileSlots = [0.5, 1, 2, 4];
 
     constructor(
         private formBuilder: FormBuilder,
