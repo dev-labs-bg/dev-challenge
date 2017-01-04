b17929d3fa718bb6268c0a4bb6c94eb3fa15313d
superKalo <superkalo@devlabs.bg>
2017-01-04 19:29:56 +0200

    Fixed: indentation; Changed: title

--- src/app/contributions/contributions.component.ts
+++ src/app/contributions/contributions.component.ts
@@ -1,26 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'xp-contributions',
-  template: `
-    <h1>Greetings, friend</h1>
-    <p>Here you could fill out some interesting information about yourself and earn some experience points while you're at it!</p>
-    <p>Each column below represents something that we value highly. Feel free to fill them in and gain some points after the admin validates. :)</p>
-    <div class="form-group">
-        <xp-stack-overflow></xp-stack-overflow>
-        <xp-public-activity></xp-public-activity>
-        <xp-side-project></xp-side-project>
-        <xp-open-source></xp-open-source>
-    </div>
-  `,
-  styles: []
+    selector: 'xp-contributions',
+    template: `
+        <h1>Bonus Experience Points</h1>
+        <p>Here you could fill out some interesting information about yourself and earn some experience points while you're at it!</p>
+        <p>Each column below represents something that we value highly. Feel free to fill them in and gain some points after the admin validates. :)</p>
+        <div class="form-group">
+                <xp-stack-overflow></xp-stack-overflow>
+                <xp-public-activity></xp-public-activity>
+                <xp-side-project></xp-side-project>
+                <xp-open-source></xp-open-source>
+        </div>
+    `,
+    styles: []
 })
-export class ContributionsComponent implements OnInit {
+export class ContributionsComponent {
 
     constructor() {}
-
-    ngOnInit() {
-        //
-    }
 
 }
